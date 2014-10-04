a529b768061c88f828c9645f02d126a5311b3280
Prier <Prier@users.noreply.github.com>
2014-10-04 12:45:04 +0200

    Changed python server and client to XML-RPC.

--- Server.py
+++ Server.py
@@ -1,131 +1,43 @@
 __author__ = 'prier'
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from SocketServer import ThreadingMixIn
-import cgi
-import random
+import argparse
 import threading
-from xml.etree.ElementTree import Element, SubElement, Comment, tostring, XML
-from xml.etree import ElementTree
-from xml.dom import minidom
-import sys
-import datetime
-import re
-lastNum = re.compile(r'(?:[^\d]*(\d+)[^\d]*)+')
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-ord_num = "00001"
-last_ord_num = "00001"
+# MES functions
 
-HOST = 'localhost'
-PORT = 21212
+def get_orders(x,y):
+    return x+y
 
-def increment(s):
-    """ look for the last sequence of number(s) in a string and increment """
-    m = lastNum.search(s)
-    if m:
-        next = str(int(m.group(1))+1)
-        start, end = m.span(1)
-        s = s[:max(end-len(next), start)] + next + s[end:]
-    return s
+def status(cell_id, event, time):
+    stat = " Cell ID: "+cell_id+"\n Event:"+event+"\n Time: "+time
+    print stat
 
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
-    rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
+class ServerThread(threading.Thread):
+    def __init__(self, server_addr):
+        threading.Thread.__init__(self)
+        self.server = SimpleXMLRPCServer(server_addr, logRequests=True, allow_none=True)
+        self.server.register_multicall_functions()
+        self.server.register_function(status, 'status')
+    def run(self):
+        self.server.serve_forever()
 
-def gen_new_order():
-    global ord_num, last_ord_num
-    ord_num = last_ord_num
-    last_ord_num = increment(ord_num)
-    ord_xxxxx = SubElement(orders, 'ord_'+ord_num)
+def run_server(host, port):
+    # server code
+    server_addr = (host, port)
+    server = ServerThread(server_addr)
+    server.start() # The server is now running
+    print '\n####### MES-server 2014 #######\n'
+    print "Server thread started. Testing the server..."
 
-    order = SubElement(ord_xxxxx, 'order')
-
-    time = SubElement(order, 'time')
-    time.text = str(datetime.datetime.now())
-
-    bricks = SubElement(order, 'bricks')
-    red = SubElement(bricks, 'red')
-    red.text = str(random.randint(1, 5))
-    yellow = SubElement(bricks, 'yellow')
-    yellow.text = str(random.randint(1, 5))
-    green = SubElement(bricks, 'green')
-    green.text = str(random.randint(1, 5))
-
-    status = SubElement(order, 'status')
-    status.text = "ready"
-
-    #print prettify(orders)
-    return
-
-
-############################################################################
-'''  One instance per connection.
-     Override handle(self) to customize action. '''
-
-class Handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/xml; charset="utf-8"')
-        self.end_headers()
-        message = threading.currentThread().getName()
-
-        if self.path == '/orders':
-            self.wfile.write(prettify(orders))
-            self.wfile.write('\n')
-        elif self.path == '/':
-            self.wfile.write(message)
-            self.wfile.write('\n')
-
-        return
-
-    def do_POST(self):
-        # Parse the form data posted
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
-
-        # Begin the response
-        self.send_response(301)
-        self.end_headers()
-        self.wfile.write('Client: %s\n' % str(self.client_address))
-        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
-        self.wfile.write('Path: %s\n' % self.path)
-        self.wfile.write('Form data:\n')
-        return
-
-############################################################################
-
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-# Ctrl-C will cleanly kill all spawned threads
-    daemon_threads = True
-    # much faster rebinding
-    allow_reuse_address = True
-
-############################################################################
+def main():
+    parser = argparse.ArgumentParser(description='Multithreaded MES XMLRPC Server')
+    parser.add_argument('--host', action="store", dest="host", default='localhost')
+    parser.add_argument('--port', action="store", dest="port", default=8000, type=int)
+    # parse arguments
+    given_args = parser.parse_args()
+    host, port = given_args.host, given_args.port
+    run_server(host, port)
 
 if __name__ == "__main__":
-
-    orders = Element('orders')
-
-    gen_new_order()
-    gen_new_order()
-    gen_new_order()
-
-    print 'Printing generated orders: \n'
-    print prettify(orders)
-
-    server = ThreadedHTTPServer((HOST, PORT), Handler)
-    # terminate with Ctrl-C
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        sys.exit(0)
-
-############################################################################
+    main()
