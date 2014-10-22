4cadf0d5d4b4d43b1db8b03b9843df586cc3a222
Kristjan Lundin <nigathr@gmail.com>
2014-10-22 10:57:39 +0200

    Seperated client into to files

--- ClientCell.py
+++ ClientCell.py
@@ -17,16 +17,6 @@ cell_status = {
     'status': "Human readable status message.."
 }
 
-mobile_status = {
-    'version_id': 1,
-    'robot_id': 3,
-    'state': 'STATE_FREE',
-    'time': str(today),
-    'battery': 80,
-    'position': "Station3",
-    'status': "Human readable status message.."
-}
-
 done = False
 sortingdone = 0
 loadingdone = 0
@@ -85,63 +75,8 @@ def emulate_cell_robot(c_response):
         print "Workcell state is: 'STATE_ORDERSORTED'"
 
 
-def emulate_mobile_robot(m_response):
-    global done
-
-    # Command state-machine for mobile robot
-
-    print "Received Mobile command: " + m_response['command']
-
-    if m_response['command'] == 'COMMAND_WAIT':
-        mobile_status['state'] = 'STATE_FREE'
-
-    elif m_response['command'] == 'COMMAND_NAVIGATE':
-        mobile_status['state'] = 'STATE_WORKING'
-
-    elif m_response['command'] == 'COMMAND_TIP':
-        mobile_status['state'] = 'STATE_WORKING'
-
-    elif m_response['command'] == 'COMMAND_ABORT':
-        mobile_status['state'] = 'STATE_FREE'
-
-
-    # State-machine for the current mobile robot state
-    if mobile_status['state'] == 'STATE_FREE':
-        print "Mobile state is: 'STATE_FREE'"
-
-    elif mobile_status['state'] == 'STATE_WORKING' and not done:
-        print "Mobile state is: 'STATE_WORKING'"
-        if m_response['command'] == 'COMMAND_TIP':
-            # Tip of those bricks!
-            print 'Tipping off bricks...'
-            time.sleep(1)
-            print 'The bricks are tipped off!'
-        else:
-            # Drive to next position
-            time.sleep(1)
-            print 'Position: ', m_response['path'], ' is reached!'
-            mobile_status['position'] = m_response['path']
-            done = True
-
-    if done:
-        print "Im am done, switching to STATE_FREE"
-        mobile_status['state'] = 'STATE_FREE'
-        done = False
-
-
-def emulate_workcell():
-    print 'Emulating workcell..'
-
-
 def main():
     while True:
-        print
-        print "Mobile state is: " + mobile_status['state']
-        # Robot sends its status to MES-server every 2 seconds
-        print server.print_state({'robot': 'Mobile3'})['response']
-        mobile_response = (server.mobile_status(mobile_status))
-        emulate_mobile_robot(mobile_response)
-        print server.print_state({'robot': 'Mobile3'})['response']
         # Workcell sends its status to MES-server every 2 seconds
         print
         print "Workcell state is: " + cell_status['state']
